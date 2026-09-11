fc39a5d59f19fa8d07962e3de88d38fce100b714
GliTcHZzZ67 <kenzie.suki67@gmail.com>
2026-09-11 07:34:31 -0700

    Add package: nano-8.1

--- scripts/generate-repo.sh
+++ scripts/generate-repo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # generate-repo.sh - Regenerate repodata.txt from packages in x86_64/
-# Copyright (C) 2026 GliTcHZzZ67
 
 set -e
 
@@ -25,12 +24,23 @@ for pkg in "$REPO_DIR"/*.mypkg.tar.xz; do
     filename=$(basename "$pkg")
     pkg_info=$(echo "$filename" | sed 's/\.mypkg\.tar\.xz$//')
 
+    # Detect arch
     arch=$(echo "$pkg_info" | grep -oE '(x86_64|aarch64|i686|armv7h)' | head -1)
     [ -z "$arch" ] && arch="x86_64"
 
+    # Strip arch suffix
     name_ver_rev=$(echo "$pkg_info" | sed "s/\.${arch}$//")
-    name=$(echo "$name_ver_rev" | sed 's/-[0-9].*//')
-    version=$(echo "$name_ver_rev" | sed "s/^${name}-//")
+
+    # Extract name (everything before first digit that starts a version)
+    name=$(echo "$name_ver_rev" | sed 's/-[0-9][0-9.]*.*$//')
+    version_part=$(echo "$name_ver_rev" | sed "s/^${name}-//")
+
+    # Version-Rev format: 2.12.3 or 2.12.3-2
+    if [[ "$version_part" == *-* ]]; then
+        version="$version_part"
+    else
+        version="${version_part}-1"
+    fi
 
     sha256=$(sha256sum "$pkg" | cut -d' ' -f1)
     url="$BASE_URL/$filename"
